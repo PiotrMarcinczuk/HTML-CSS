ce6385d68e141e0219b2b9f4604f5727aefc3157
dzw <100282489+PiotrMarcinczuk@users.noreply.github.com>
2023-02-02 19:32:32 +0100

    bud

---
+++ BUDOWA/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+	<link rel="stylesheet" href="styl.css" type="text/css">
+	<title>Usługi remontowe</title>
+</head>
+<body>
+    <div id="container">
+	    <div id="naglowek">
+		    <div id="baner">
+		    <div id="foto">
+			    <img src="ddd.jpg" alt="Dombud">
+			</div>
+		    <div id="odnosniki">
+			    <a href="index.php">Home</a>
+			    <a href="O_nas.php">Onas</a>
+			    <a href="Uslugi.php">Usługi</a>
+			    <a href="Realizacje.php">Realizacje</a>
+			    <a href="Kontakt.php">Kontakt</a>
+			</div>
+		    </div>
+		    <div id="opis">
+		        <p>Posiadamy bogatą ofertę usług budowlanych. Realizujemy zlecenia na dużych budynkach mieszkalnych jak również remontujemy mniejsze budowle. 
+				Współpracujemy ze Wspólnotami Mieszkaniowymi jak również z klientami prywatnymi.</p>
+		    </div>
+		</div>
+		<div id="main">
+		    <div class="oferta">
+		        <img src="dom1.jpg" alt="dom"><br>
+		        <p>Domy i mieszkania</p>
+				<ul>
+				    <li>Kompleksowy remont łazienki</li>
+					<li>Remont ścian i sufitów</li>
+					<li>Przeróbki i montaż instalacji elektrycznej</li>
+					<li>Przeróbki instalacji wodno-kanalizacyjnej</li>
+					<li>Pełna stolarka</li>
+					<li>Termomodernizacja</li>
+					<li>Układanie glazury i terakoty</li>
+					<li>Malowanie natryskowe i tradycyjne</li>
+					<li>Wyburzenia i rozbiórki</li>
+				</ul>
+		    </div>
+		    <div class="oferta">
+			    <img src="dom2.jpg" alt="lokale"><br>
+			    <p>Lokale mieszkalne</p>
+				<ul>
+				    <li>Adaptacja przebudowa powierzchni użytkowych zgodnie zich zaplanowanym przeznaczeniem</li>
+					<li>Rewitalizacja – obejmująca całkowite odnowienie budynku lub generalny remont lokalu.</li>
+				</ul>
+		    </div>
+		    <div class="oferta">
+			    <img src="dom3.jpg" alt="publiczne"><br>
+			    <p>Budynki publiczne</p>
+				<ul>
+				    <li>Całkowite remonty i modernizacje obiektów użyteczności publicznej</li>
+					<li>Wymiana stolarki okiennej i drzwiowej</li>
+					<li>Dostosowanie budynków do panujących norm i przepisów</li>
+					<li>Osuszanie po zalaniu</li>
+					<li>Sufity podwieszane</li>
+				</ul>
+		    </div>
+		</div>
+		<div id="stopka">
+		    <div class="footer">
+			    <h4>Kontakt</h4>
+				<p>Nr. telefonu: +48 691 257 812</p>
+				<p>e-mail: <a href="sznycmail.com">Sznycowanie@gamail.com</a></p>
+				<p>Adres: Kurznie ul.twojego starego 18</p>
+			</div>
+			<div class="footer">
+			    <h4>Media społecznościowe</h4>
+				<a href="https://www.instagram.com/haniaa.rozanska">Instagram</a>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
