40f5cdd5d2d14066bad59174e12c2bd578b02566
dzw <100282489+PiotrMarcinczuk@users.noreply.github.com>
2023-03-08 00:43:42 +0100

    08.03

--- SznycWiki/gallery_script.js
+++ SznycWiki/gallery_script.js
@@ -1,11 +1,35 @@
-let elements = document.querySelectorAll(".foto");
+let gallery = document.querySelectorAll(".foto");
 let selected = document.querySelector("#selected_foto");
-let selected_img = selected.querySelector("img");
+let selected_img = document.querySelector("#main_foto");
+let current;
 
-elements.forEach((element) => {
+let right = document.querySelector("#right");
+let left = document.querySelector("#left");
+
+gallery.forEach((element, index) => {
     element.addEventListener("click", (img_target) => {
         selected.style.display = "flex";
         selected_img.src = img_target.target.src;
+        current = index;
     })
 })
+
+function close_selected(){
+    selected.style.display = "none";
+}
+
+function next(){
+    current += 1;
+    selected_img.src = gallery[current].querySelector("img").src;
+}
+
+function prev(){
+    current -= 1;
+    selected_img.src = gallery[current].querySelector("img").src;
+}
+
+
+
+
+
 
