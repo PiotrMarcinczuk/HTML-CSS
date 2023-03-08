efac35b0dab546bc625f93a28c922e35eedeb879
dzw <100282489+PiotrMarcinczuk@users.noreply.github.com>
2023-03-08 18:09:23 +0100

    08.03

--- SznycWiki/gallery_script.js
+++ SznycWiki/gallery_script.js
@@ -1,16 +1,23 @@
 let gallery = document.querySelectorAll(".foto");
 let selected = document.querySelector("#selected_foto");
 let selected_img = document.querySelector("#main_foto");
+let podpis = document.querySelectorAll('.podpis');
+
+let figcaption = document.querySelectorAll('.napis_fig');
+let napis = document.querySelector('.napis');
 let current;
 
 let right = document.querySelector("#right");
 let left = document.querySelector("#left");
+
+
 
 gallery.forEach((element, index) => {
     element.addEventListener("click", (img_target) => {
         selected.style.display = "flex";
         selected_img.src = img_target.target.src;
         current = index;
+        napis.textContent = podpis[index].textContent;
     })
 })
 
@@ -22,21 +29,24 @@ function next(){
     if(current == gallery.length-1){
         current = 0;
         selected_img.src = gallery[current].querySelector("img").src;
+        napis.textContent = podpis[current].textContent;
     }
     else{
         current += 1;
         selected_img.src = gallery[current].querySelector("img").src;
+        napis.textContent = podpis[current].textContent;
     }
 }
-
 function prev(){
     if(current == 0){
         current = gallery.length-1;
         selected_img.src = gallery[current].querySelector("img").src;
+        napis.textContent = podpis[current].textContent;
     }
     else{
         current -= 1;
         selected_img.src = gallery[current].querySelector("img").src;
+        napis.textContent = podpis[current].textContent;
     }
 }
 
