507cc65b06ceeb4e41c1ca60b2f11f298d24e469
dzw <100282489+PiotrMarcinczuk@users.noreply.github.com>
2023-04-05 18:46:01 +0200

    Kalkulator skrypt dodawanie wpisywanie w previous i czyszczenie w niektórcyh wypadkach

--- calc/script.js
+++ calc/script.js
@@ -3,13 +3,14 @@ const current = document.querySelectorAll('#current');
 
 const sign = document.querySelectorAll('.sign');
 const number = document.querySelectorAll('.number');
-const equal = document.querySelectorAll('#equal');
+const equal = document.querySelectorAll('.equal');
 
 let p_current = document.querySelector("#current p")
 let p_previous = document.querySelector("#previous p")
 let current_number = "";
 
-sign.forEach((button) => button.addEventListener('click', operate))
+sign.forEach((button) => button.addEventListener('click', operate));
+equal.forEach((button) => button.addEventListener('click', math));
 
 number.forEach((button) => {
     button.addEventListener('click', display_number);
@@ -18,17 +19,40 @@ number.forEach((button) => {
 function display_number(){
     if(this.textContent === '.' && p_current.textContent === ''){
         p_current.textContent = "0.";
-        return;
     }else if(p_current.textContent.includes('.')){
-        return
+        if(this.textContent !== '.'){
+            p_current.textContent += this.textContent;
+        }else{
+            return;
+        }
+    }else if(p_previous.textContent.includes('=')){
+        p_current.textContent = '';
+        p_previous.textContent = '';
+        p_current.textContent = this.textContent;
     }
-    p_current.textContent += this.textContent;
-    
+    else{
+        p_current.textContent += this.textContent;
+    }
 }
 
 function operate(){
     if(p_current.textContent !== ''){
-        p_previous.textContent = this.textContent;
+        p_previous.textContent = p_current.textContent;
+        p_previous.textContent += this.textContent;
+        p_current.textContent = '';
     }
+}
+
+function math(){
+    if(parseFloat(p_current.textContent) < Infinity){
+        if(p_previous.textContent.includes('+') && !p_previous.textContent.includes('=')){
+            let temp = parseFloat(p_current.textContent);
+            p_current.textContent = parseFloat(p_previous.textContent) + parseFloat(p_current.textContent);
+            p_previous.textContent += temp + ' =';
+        }
+    }else{
+        p_current.textContent = "Nieskończoność";
+    }
+    
 }
 
