63517d384c86c2b99d19d1acde7ae7d5ba7a59e5
dzw <100282489+PiotrMarcinczuk@users.noreply.github.com>
2023-04-10 22:06:21 +0200

    Duzo dorbnych zmian zabawa z potegowanie złożone działania

--- calc/script.js
+++ calc/script.js
@@ -11,10 +11,12 @@ const p_previous_2 = document.querySelector("#number_2");
 const operator_previous = document.querySelector("#operator");
 const equal_previous = document.querySelector("#equal_previous");
 const all = document.querySelectorAll('.all');
+
 let current_number = 0;
+let temp_operator = '';
+let temp_sqr = '';
 
 sign.forEach((button) => button.addEventListener('click', operate));
-
 equal.forEach((button) => button.addEventListener('click', math));
 
 number.forEach((button) => {
@@ -30,8 +32,12 @@ function remove(){
 }
 
 function display_number(){
-    if(this.textContent === '.' && p_current.textContent === ''){
-        p_current.textContent = "0.";
+    if(this.textContent === '.' && (p_current.textContent === '' || p_current.textContent === '-')){
+        if(p_current.textContent === '-'){
+            p_current.textContent = "-0.";
+        }else{
+            p_current.textContent = "0.";
+        }
     }else if(p_current.textContent.includes('.')){
         if(this.textContent !== '.'){
             p_current.textContent += this.textContent;
@@ -51,33 +57,61 @@ function display_number(){
     }
 }
 
-function operate(){
+function operate(){  
+    current_number = p_current.textContent;
     if(p_current.textContent !== ''){
-        if(operator_previous.textContent === ''){
+        console.log(1);
+        if(p_previous_2.textContent === '' && equal_previous.textContent === ''){
+            if(this.textContent !== 'x2'){
+                p_previous.textContent = p_current.textContent;
+            }
+            console.log(2);
+            if(p_previous.textContent === '-'){
+                console.log(2.2);
+                operator_previous.textContent = '';
+            }else if(this.textContent !== 'x2'){
+                console.log(2.3);
+                temp_operator = this.textContent;
+                operator_previous.textContent = this.textContent;
+            }
+            if(p_current.textContent === '-' && p_previous.textContent === '-'){
+                console.log(2.4);
+                p_previous.textContent = '';
+            }
+            p_current.textContent = '';
+        }
+        else if(operator_previous.textContent === ''){
+            console.log(3);
             p_previous.textContent = p_current.textContent;
             operator_previous.textContent += this.textContent;
             p_current.textContent = '';
         }else if(p_previous !== ''){
+            console.log(4);
             p_previous.textContent = p_current.textContent;
             operator_previous.textContent = this.textContent;
             p_previous_2.textContent = '';
             equal_previous.textContent = '';
             p_current.textContent = '';
         }
+        if(this.textContent === 'x2'){
+            console.log(5);
+            if(p_previous.textContent === ''){
+                temp_sqr = current_number;
+                operator_previous.textContent = 'sqr';
+                p_previous_2.textContent = '('+temp_sqr+')';
+                p_current.textContent = '';
+                math();
+            }else{
+                temp_sqr = current_number;
+                operator_previous.textContent = temp_operator + 'sqr';
+                p_previous_2.textContent = '('+temp_sqr+')';
+                p_current.textContent = '';
+            }
+        }
+    }else if(p_current.textContent === '' && this.textContent === '-'){
+        p_current.textContent = '-';
     }else{
         previous.textContent = '';
-    }
-
-    if(this.textContent === 'x2' && p_current.textContent === ''){
-        p_previous.style.display = 'none';
-        let temp_previous  = p_previous.textContent;
-        operator_previous.textContent = 'sqr(' + temp_previous + ')';
-        p_current.textContent = '';
-        math();
-        
-    }
-    if(this.textContent === '√x'){
-        operator_previous.textContent = '√';
     }
 }
 
@@ -114,6 +148,7 @@ function math(){
                     p_previous.textContent = temp_p_previous;
                     temp_p_current = parseFloat(p_previous.textContent) - parseFloat(p_previous_2.textContent);
                     p_current.textContent = temp_p_current;
+                    break;
                 }
             case('×'):
                 if(equal_previous.textContent === ''){
@@ -129,6 +164,7 @@ function math(){
                     p_previous.textContent = temp_p_previous;
                     temp_p_current = parseFloat(p_previous.textContent) * parseFloat(p_previous_2.textContent);
                     p_current.textContent = temp_p_current;
+                    break;
                 }
             case('÷'):
                 if(equal_previous.textContent === ''){
@@ -144,15 +180,8 @@ function math(){
                     p_previous.textContent = temp_p_previous;
                     temp_p_current = parseFloat(p_previous.textContent) / parseFloat(p_previous_2.textContent);
                     p_current.textContent = temp_p_current;
-                }
-            case('sqr(' + p_previous.textContent + ')'):
-                if(equal_previous.textContent === ''){
-                    p_current.textContent = parseFloat(p_previous.textContent) * parseFloat(p_previous.textContent)
-                    current_number = parseFloat(p_current.textContent);
-                    console.log(p_current.textContent)
                     break;
-               }
-
+                }
         }
     }
     
