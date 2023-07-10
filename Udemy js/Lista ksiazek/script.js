db982b79443d53b4aff4a78ef19b7a1161496899
dzw <100282489+PiotrMarcinczuk@users.noreply.github.com>
2023-07-10 18:05:30 +0200

    Project 1 lista ksiazek

--- Udemy js/Lista ksiazek/script.js
+++ Udemy js/Lista ksiazek/script.js
@@ -41,32 +41,58 @@ class BookList{
     addElement(book){
         const main = document.querySelector('main');
         let elementDiv = document.createElement('div');
-        let elementTable = document.createElement('table');
+        let elementTable = document.querySelector('table');
+
         let elementCol = document.createElement('tr');
         let elementRow1 = document.createElement('td');
         let elementRow2 = document.createElement('td');
         let elementRow3 = document.createElement('td');
         let elementRow4 = document.createElement('td');
-        let button1 = document.createElement('button');
-        let button2 = document.createElement('button');
-        button1.innerText = 'Up';
-        button2.innerText = 'Remove';
+        let buttonUp = document.createElement('button');
+        let buttonRemove = document.createElement('button');
+        buttonUp.innerText = 'Up';
+        buttonRemove.innerText = 'Remove';
         elementDiv.appendChild(elementTable);
         elementTable.appendChild(elementCol);
         elementCol.appendChild(elementRow1);
         elementCol.appendChild(elementRow2);
         elementCol.appendChild(elementRow3);
         elementCol.appendChild(elementRow4);
-        elementRow3.appendChild(button1);
-        elementRow4.appendChild(button2);
+        elementRow3.appendChild(buttonUp);
+        elementRow4.appendChild(buttonRemove);
 
         elementRow1.innerHTML = 'Imię autora: ' + book.name; 
         elementRow2.innerHTML = 'Id książki : ' + book.id;
-
-        button2.addEventListener('click', (e) => { // usuwanie pr zyciskiem remove
-            elementTable.remove(elementCol);
-        });
         main.appendChild(elementDiv);
+
+        buttonRemove.addEventListener('click', () => { // usuwanie pr zyciskiem remove
+            //usuwamy tez z localStorage
+            const t = storageTemp.getItems();
+            for(let [key, value] of t.entries()){
+                if(value.name === book.name){
+                    elementTable.remove(elementCol);
+                    bookList.list.splice(key,1);
+                }
+            }
+            this.saveData();
+        });
+
+        buttonUp.addEventListener('click', () => {
+            const t = storageTemp.getItems();
+            for(let [key, value] of this.list.entries()){
+                if(value.listId === book.listId){
+                    if(key >= 1){
+                        console.log(this.list[key]);
+                        let temp = this.list[key-1];               
+                        this.list[key-1] = this.list[key];
+                        this.list[key] = temp;
+                    }
+                }   
+            }
+            this.saveData();
+            this.deleteAll();
+            this.loadDataFromStorage();
+        })
     }
 
     addToList(book){
@@ -102,6 +128,13 @@ class BookList{
     saveData(){
         storageTemp.setItems(this.list);
     }
+
+    deleteAll(){
+        const tableRows = document.querySelectorAll('table tr');
+        tableRows.forEach( (el) => {
+            el.remove();
+        })
+    }
 }
 const bookList = new BookList();
 
@@ -109,7 +142,15 @@ class Book{
     constructor(name, id){
         this.name = name;
         this.id = id;
+        this.listId = Book.getNextId();
     }    
+
+    static listId = 0;
+    static getNextId(){
+        const id = Book.listId;
+        Book.listId++;
+        return id;
+    }
 }
 
 class Storage{
@@ -129,4 +170,3 @@ class Storage{
     }
 }
 const storageTemp = new Storage();
-
