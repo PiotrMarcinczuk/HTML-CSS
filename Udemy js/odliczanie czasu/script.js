a9d834d2545c922c9b4e8d45c23a37b739adc83b
dzw <100282489+PiotrMarcinczuk@users.noreply.github.com>
2023-07-12 18:32:56 +0200

    Mini Project 2 odliczanie

--- Udemy js/odliczanie czasu/script.js
+++ Udemy js/odliczanie czasu/script.js
@@ -1,15 +1,42 @@
 const button = document.getElementById('countdown-button');
+const dateTimeButton = document.getElementById('timer');
 let intervalId = 0;
-button.addEventListener('click', startInterval)
+let finalTime = null;
+
+button.addEventListener('click', init)
+dateTimeButton.addEventListener('input', enableButton);
+
+function enableButton(){
+    button.disabled = false;
+}
+
+loadFromStorage();
 
 function init(){
-    let timeValue = document.getElementById('timer').value;
-    let finalTime = new Date(timeValue);
+    finalTime = document.getElementById('timer').value;
+    if(finalTime == '') return;
+    finalTime = new Date(finalTime);
 
-    let currentDate = new Date();
-    let current = currentDate.getTime();
+    timer();
+    timerId = setInterval(timer, 1000);
+}
 
-    let result = finalTime - current;
+function timer(){
+    const current = new Date().getTime();
+    let result = null;
+    if(localStorage.getItem('finalTime') !== null){
+        let temp = JSON.parse(localStorage.getItem('finalTime'));
+        let finalTemp = new Date(temp);
+        saveStorage(finalTemp, current);
+        loadFromStorage();
+    }else{
+        result = finalTime - current;
+        math(result)
+        saveStorage(finalTime, current);
+    }
+}
+
+function math(result){
     result = Math.floor(result / 1000)
 
     const pDays = document.getElementById('days');
@@ -18,26 +45,48 @@ function init(){
 
     const pHours = document.getElementById('hours');
     let hours = Math.floor(result % ((60 * 60) * 24) / (60 * 60));
-    pHours.innerHTML = hours;
+   pHours.innerHTML = hours;
 
-    const pMinutes = document.getElementById('minutes');
+   const pMinutes = document.getElementById('minutes');
     let minutes = Math.floor((result % (60 * 60)) / 60);
     pMinutes.innerHTML = minutes;
 
     const pSeconds = document.getElementById('seconds');
     let seconds = Math.floor(result % 60);
     pSeconds.innerHTML = seconds;
+
     if(result <= 0){
+        console.log(2);
         clearInterval(intervalId);
         let div = document.getElementById('messege');
-        div = div.style.display = 'block';
+        div.style.display = 'block';
     } 
-    console.log(result);
 }
 
-function startInterval(){
-    let div = document.getElementById('messege');
-    div = div.style.display = 'none';
-    intervalId = setInterval(init, 1000);
+function saveStorage(finalTime, currentTime){
+    if(finalTime !== 'null'){
+        localStorage.setItem('finalTime', JSON.stringify(finalTime));
+        localStorage.setItem('currentTime', JSON.stringify(currentTime));
+    }
 }
+
+function loadFromStorage(){
+    let storedfinalTime = localStorage.getItem('finalTime');
+    if(storedfinalTime){
+        const finalTimeTemp = new Date(JSON.parse(storedfinalTime));
+        const currentTimeTemp = new Date().getTime();
+        const result = finalTimeTemp - currentTimeTemp; 
+        if(result > 0){
+            math(result);
+            saveStorage(finalTimeTemp, currentTimeTemp);
+            intervalId = setInterval(timer, 1000);
+        }else{
+            clearInterval(intervalId);
+            let div = document.getElementById('messege');
+            div.style.display = 'block';
+        }
+        return finalTimeTemp;
+    }
+}
+
 
