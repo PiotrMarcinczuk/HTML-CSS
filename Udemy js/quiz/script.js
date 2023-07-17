b58bf230fb999ba6316b291fb612812b621c431d
dzw <100282489+PiotrMarcinczuk@users.noreply.github.com>
2023-07-17 14:40:05 +0200

    Mini Project 3 quiz

--- Udemy js/quiz/script.js
+++ Udemy js/quiz/script.js
@@ -1,24 +1,28 @@
+const title = document.querySelector('main h1')
+const a1 = document.getElementById('label_answer1')
+const a2 = document.getElementById('label_answer2')
+const a3 = document.getElementById('label_answer3')
+
+const button = document.querySelector('#quiz-box button');
+
 class Quiz{
     constructor(){
         this.questionList = [
-            {question: 'Ile kg waży Sznyc?', answer1: 55, answer2: 88, answer3: 100, correctAnswer: answer2, flag: null},
-            {question: 'Jak nazywa się Sznyc?', answer1: 'Wojtek', answer2: 'Karol', answer3: 'Bożydar', correctAnswer: answer1, flag: null}
-        ]
+            {question: 'Ile kg waży Sznyc?', answer1: 55, answer2: 88, answer3: 100, correctAnswer: answer2, flag: false},
+            {question: 'Jak nazywa się Sznyc?', answer1: 'Wojtek', answer2: 'Karol', answer3: 'Bożydar', correctAnswer: answer1, flag: false},
+            {question: 'Jak na imię miał Jan Paweł II?', answer1: 'Magdalena', answer2: 'Jan', answer3: 'Karol', correctAnswer: answer3, flag: false}
 
-        this.number = null;
+        ]
     }
 
-    getRandomNumber(){
-        this.number = Math.floor(Math.random() * this.questionList.length);
+    getRandomNumber(len){
+        return Math.floor(Math.random() * len);
     }
 
     getHtmlElements(){
-        const title = document.querySelector('main h1')
-        const a1 = document.getElementById('label_answer1')
-        const a2 = document.getElementById('label_answer2')
-        const a3 = document.getElementById('label_answer3')
-
-        const questionTemp = this.getQuestion();
+        let questionTemp = this.getQuestion();
+        console.log(questionTemp);
+        console.log(this.number);
         
         title.innerHTML = questionTemp.question;
         a1.innerHTML = questionTemp.answer1;
@@ -26,18 +30,34 @@ class Quiz{
         a3.innerHTML = questionTemp.answer3;
     }
 
-    getQuestion(){
-        this.getRandomNumber();
-        for(let [key, value] of this.questionList.entries()){
-            
-            if(key === this.number && value.flag === null){
-                value.flag = true;
-                return value;
-            }
+    getQuestion(){  
+        let unanswered = this.questionList.filter( (e) => !e.flag)
+        if(unanswered.length === 0){
+            this.getRandomNumber();
+            return this.getQuestion()
         }
+
+        let index = this.getRandomNumber(unanswered.length);
+        let question = unanswered[index];
+        question.flag = true;
+        return question;
+    }
+
+    nextQuestion(){
+        quiz.getHtmlElements();
+        quiz.clearCheckbox();
+    }
+
+    clearCheckbox(){
+        const checkbox = document.querySelectorAll('input[type="checkbox"]');
+        checkbox.forEach( (el) => {
+            el.checked = false;
+        })
+   
     }
 }
 
 quiz = new Quiz();
 quiz.getHtmlElements();
+button.addEventListener('click', quiz.nextQuestion);
 
