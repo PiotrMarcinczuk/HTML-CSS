7717500e4134e84d897e7abf8cdd41a2aea5e3ef
dzw <100282489+PiotrMarcinczuk@users.noreply.github.com>
2023-07-26 15:48:14 +0200

    Nauka

---
+++ Udemy js/node/serwerowanie plikow/node.js
@@ -0,0 +1,23 @@
+let http = require('http');
+let url = require('url');
+let fs = require('fs') // file sysyem
+http.createServer(
+    function(req, res){
+        res.writeHead(200, {'Content-type': 'text/html'});
+        let fileName = './public' + url.parse(req.url, true).pathname;
+
+        fs.readFile(fileName, function(err, data){
+            if(err){
+                res.writeHead(404, {'Content-type': 'text/html'});
+                return res.end('404 file not found');
+            }
+
+            
+            res.writeHead(200, {'Content-type': 'text/html'});
+
+            res.write(data);
+            res.end();
+        })
+
+    }
+).listen(8080)
