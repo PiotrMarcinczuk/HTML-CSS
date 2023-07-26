17fca38c34867bbedffbe8659b87b235f1244959
dzw <100282489+PiotrMarcinczuk@users.noreply.github.com>
2023-07-26 17:10:55 +0200

    Nauka

--- Udemy js/node/serwerowanie plikow/node.js
+++ Udemy js/node/serwerowanie plikow/node.js
@@ -1,23 +1,21 @@
 let http = require('http');
 let url = require('url');
-let fs = require('fs') // file sysyem
+let fs = require('fs');
+
 http.createServer(
     function(req, res){
-        res.writeHead(200, {'Content-type': 'text/html'});
-        let fileName = './public' + url.parse(req.url, true).pathname;
+        res.writeHead(200, {'Content-type':'text/html'});
+        let temp = './public' + url.parse(req.url, true).pathname;
 
-        fs.readFile(fileName, function(err, data){
+        fs.readFile(temp, function(err, data){
             if(err){
-                res.writeHead(404, {'Content-type': 'text/html'});
+                res.writeHead(404, {'Content-type':'text/html'});
                 return res.end('404 file not found');
             }
 
-            
-            res.writeHead(200, {'Content-type': 'text/html'});
-
+            res.writeHead(200, {'Content-type':'text/html'});
             res.write(data);
             res.end();
         })
-
     }
-).listen(8080)
+).listen(8080);
